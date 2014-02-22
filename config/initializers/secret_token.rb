cf79259999e24ee44dda85a88f8a147d8423fb87
durjeibis <durje03@gmail.com>
2014-02-22 15:44:51 -0500

    creacion de proyecto e instalacion de foundation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taskmanager::Application.config.secret_token = 'a414405a68cb7dcab2bb9d768026ce0437d6355a6adfbc937914251d11e08474206cb7a63ca0a3fdfc3f8f8f7d0647b7a6603411b24e9293aab167b502dc9432'
